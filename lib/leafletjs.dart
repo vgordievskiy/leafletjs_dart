df1ffdd702b84ce612dfa4877f4f522a6a0a78f4
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-07-21 20:13:16 +0300

    changes

--- lib/leafletjs.dart
+++ lib/leafletjs.dart
@@ -1,5 +1,3 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
 library leafletjs_dart;
 
 import 'dart:html';
