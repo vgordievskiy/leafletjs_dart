16d8ca098b10d539ecfa35b91ad8acfe438c9b00
gardi <v.gordievskiy@gmail.com>
2015-07-22 01:08:00 +0000

    add LatLngBounds

--- lib/leafletjs_js_bindings/geo.dart
+++ lib/leafletjs_js_bindings/geo.dart
@@ -7,7 +7,16 @@ import 'package:js_wrapping/js_wrapping.dart';
 part 'geo.g.dart';
 
 abstract class _LatLng implements JsInterface  {
-  external factory _LatLng();
+  external factory _LatLng(double lat, double lnt, [double alt = null]);
   double get lat;
   double get lng;
+}
+
+abstract class _LatLngBounds implements JsInterface {
+  external factory _LatLngBounds(LatLng SW, LatLng NE);
+  bool isValid();
+  LatLng getSouthWest();
+  LatLng getNorthEast();
+  LatLng getNorthWest();
+  LatLng getSouthEast();
 }
--- lib/leafletjs_js_bindings/geo.g.dart
+++ lib/leafletjs_js_bindings/geo.g.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// 2015-07-22T00:55:40.459Z
+// 2015-07-22T01:07:45.552Z
 
 part of leafletjs_dart.bind.geo;
 
@@ -10,7 +10,33 @@ part of leafletjs_dart.bind.geo;
 
 class LatLng extends JsInterface implements _LatLng {
   LatLng.created(JsObject o) : super.created(o);
-  LatLng() : this.created(new JsObject(context['L']['LatLng']));
+  LatLng(double lat, double lnt, [double alt = null])
+      : this.created(new JsObject(context['L']['LatLng'], [lat, lnt, alt]));
   double get lat => asJsObject(this)['lat'];
   double get lng => asJsObject(this)['lng'];
 }
+
+// **************************************************************************
+// Generator: JsInterfaceGenerator
+// Target: abstract class _LatLngBounds
+// **************************************************************************
+
+class LatLngBounds extends JsInterface implements _LatLngBounds {
+  LatLngBounds.created(JsObject o) : super.created(o);
+  LatLngBounds(LatLng SW, LatLng NE) : this.created(new JsObject(
+          context['L']['LatLngBounds'], [
+        __codec2.encode(SW),
+        __codec2.encode(NE)
+      ]));
+  bool isValid() => asJsObject(this).callMethod('isValid');
+  LatLng getSouthWest() =>
+      __codec2.decode(asJsObject(this).callMethod('getSouthWest'));
+  LatLng getNorthEast() =>
+      __codec2.decode(asJsObject(this).callMethod('getNorthEast'));
+  LatLng getNorthWest() =>
+      __codec2.decode(asJsObject(this).callMethod('getNorthWest'));
+  LatLng getSouthEast() =>
+      __codec2.decode(asJsObject(this).callMethod('getSouthEast'));
+}
+/// codec for leafletjs_dart.bind.geo.LatLng
+final __codec2 = new JsInterfaceCodec<LatLng>((o) => new LatLng.created(o));
