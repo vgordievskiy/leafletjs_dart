7180f26b93409fb48af383a8c94cde1e8ec457fd
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-07-22 16:40:17 +0300

    add Icon

---
+++ lib/leafletjs_js_bindings/Icon.dart
@@ -0,0 +1,11 @@
+@JsName('L')
+library leafletjs_dart.bind.Icon;
+
+import 'dart:html';
+import 'package:js_wrapping/js_wrapping.dart';
+
+part 'Icon.g.dart';
+
+abstract class _Icon implements JsInterface  {
+  external factory _Icon(JsObject params);
+}
---
+++ lib/leafletjs_js_bindings/Icon.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// 2015-07-22T13:30:41.702Z
+
+part of leafletjs_dart.bind.Icon;
+
+// **************************************************************************
+// Generator: JsInterfaceGenerator
+// Target: abstract class _Icon
+// **************************************************************************
+
+class Icon extends JsInterface implements _Icon {
+  Icon.created(JsObject o) : super.created(o);
+  Icon(JsObject params)
+      : this.created(new JsObject(context['L']['Icon'], [params]));
+}
