c43e57c3f4b65e277a3cc32c054652f1f2e761d4
gardi <v.gordievskiy@gmail.com>
2015-07-24 02:02:09 +0000

    add L.IHandler

---
+++ lib/leafletjs_js_bindings/IHandler.dart
@@ -0,0 +1,11 @@
+@JsName('L')
+library leafletjs_dart.bind.IHandler;
+
+import 'dart:html';
+import 'package:js_wrapping/js_wrapping.dart';
+
+part 'IHandler.g.dart';
+
+abstract class _IHandler implements JsInterface  {
+  external factory _IHandler();
+}
---
+++ lib/leafletjs_js_bindings/IHandler.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// 2015-07-24T02:01:53.534Z
+
+part of leafletjs_dart.bind.IHandler;
+
+// **************************************************************************
+// Generator: JsInterfaceGenerator
+// Target: abstract class _IHandler
+// **************************************************************************
+
+class IHandler extends JsInterface implements _IHandler {
+  IHandler.created(JsObject o) : super.created(o);
+  IHandler() : this.created(new JsObject(context['L']['IHandler']));
+}
