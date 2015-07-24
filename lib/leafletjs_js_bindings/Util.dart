269e351d924c047d8f1362d3dbb94aa18064a2d4
v.gordiedievskiy <v.gordievskiy@gmail.com>
2015-07-24 20:39:47 +0300

    init L.Util

---
+++ lib/leafletjs_js_bindings/Util.dart
@@ -0,0 +1,10 @@
+@JsName('L.Util')
+library leafletjs_dart.bind.Util;
+
+import 'dart:html';
+import 'package:js_wrapping/js_wrapping.dart';
+
+String stamp(JsObject obj) {
+  JsFunction cntx = context['L']['Util']['stamp'];
+  return cntx.apply([obj]);
+}
