ad7388db867a0f32361018671c5a865e08879f09
gardi <v.gordievskiy@gmail.com>
2016-01-23 14:09:06 +0000

    small fix

--- lib/leafletjs_js_bindings/Util.dart
+++ lib/leafletjs_js_bindings/Util.dart
@@ -2,6 +2,7 @@
 library leafletjs_dart.bind.Util;
 
 import 'dart:js';
+import 'dart:convert';
 import 'package:js/js.dart';
 
 @JS('Leaflet.Util.stamp')
@@ -9,6 +10,8 @@ external int stamp(var obj);
 
 @JS('JSON.stringify')
 external String toJSON(var obj);
+
+dynamic toDart(dynamic obj) => JSON.decode(toJSON(obj));
 
 JsObject toJs(var obj){
  return new JsObject.jsify(obj);
